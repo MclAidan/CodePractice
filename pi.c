5043ac5dbbf807664e2b336ddca87bca2489d464
MclAidan <52727819+MclAidan@users.noreply.github.com>
2021-08-30 19:45:36 -0600

    Update pi.c

--- pi.c
+++ pi.c
@@ -1,23 +1,26 @@
+//This is a program that approximates pi using only random numbers
+
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <time.h>
 
 int main()
 {
-long long itt = 100000;long long count = 0;
+long long itt = 100000; //Itteration counter
+long long count = 0;
 long p = RAND_MAX;
 
-srand(time(0));
+srand(time(0)); //Seed
 
 for (long long i = itt; i>0; --i)
 {
-	long  int1 = rand(); long  int2 = rand();
+	long  int1 = rand(); long  int2 = rand(); // Random numbers are cast as cordinates
 
-	if ((int2 * int2) + (int1 * int1) <= p*p )
+	if ((int2 * int2) + (int1 * int1) <= p*p ) // Check if in cicle
 	{
 		count = count + 1;
 	}
 }
-double res = (double)count / (double)itt *4;
+double res = (double)count / (double)itt *4; //Solve for pi
 printf("%lf\n",res );
 }
